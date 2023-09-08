782d4036fef96d7c523b3fbea77d2924d4bf9341
mennassaber <mennasaber202620@gmail.com>
2023-09-07 20:18:26 -0700

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point 
  *
- * Description: A C program that prints with put function 
+ * Description: A c program that prints with put function 
  *
  * Return: Alawys 0 (success)
 */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle"):
-    return (0):
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
 
