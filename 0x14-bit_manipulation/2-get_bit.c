87014ff2a44abb571c1e87e43c665e9395856d64
mennassaber <mennasaber202620@gmail.com>
2024-02-08 14:17:39 -0800

    git

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,33 +1,28 @@
 #include "main.h"
 
 /**
- * get_bit - Returns the value of a bit at a given index.
- * @n: The unsigned long int input.
- * @index: The index of the bit.
+ * get_bit - Get the value of a bit at a given index
+ * @n: The number to extract the bit from
+ * @index: The index of the bit to retrieve
  *
- * Return: The value of the bit at the specified index,
- *         or -1 if the index is out of range.
+ * Return: The value of the bit at index, or -1 if an error occurred
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned int i;
+    /*
+     * Check if index is within the bounds of the data type.
+     * If index is greater than or equal to the number of bits in an
+     * unsigned long int, return -1 indicating an error.
+     */
+    if (index >= (sizeof(unsigned long int) * 8))
+        return (-1);
 
-    /* Check if the index is within the range of a long int */
-    if (index < 64)
-    {
-        /* Iterate through each bit of the number */
-        for (i = 0; i <= 63; n >>= 1, i++)
-        {
-            /* Check if the current index matches the specified index */
-            if (index == i)
-            {
-                /* Return the value of the bit at the specified index */
-                return (n & 1);
-            }
-        }
-    }
-
-    /* Return -1 if the index is out of range */
-    return (-1);
+    /*
+     * Right shift the number n by index bits, effectively bringing
+     * the bit at the specified index to the least significant position,
+     * and then perform a bitwise AND operation with 1 to retrieve
+     * the value of the bit.
+     */
+    return ((n >> index) & 1);
 }
 
