82ecda7d0de5d1d77defde1ca0bf5add41feb93f
mennassaber <mennasaber202620@gmail.com>
2024-02-08 14:43:11 -0800

    git

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,24 +1,19 @@
+
 #include "main.h"
 
 /**
- * clear_bit - Sets the value of a bit to 0 at a given index.
- * @n: Pointer to an unsigned long int.
- * @index: Index of the bit to clear.
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
  *
- * Return: 1 if the operation is successful, -1 if it fails.
+ * Return: 1 for success, -1 for failure
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    /* Check if the index is within the valid range */
-    if (index > 63)
-        return (-1);
+	if (index > 63)
+		return (-1);
 
-    unsigned int mask = 1 << index; /* Create a mask with the bit to clear */
-
-    /* Check if the bit at the specified index is already 0 */
-    if (*n & mask)
-        *n ^= mask; /* Clear the bit if it is set */
-
-    return (1); /* Return 1 to indicate success */
+	*n = (~(1UL << index) & *n);
+	return (1);
 }
 
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns the number of bits needed to flip to transform
- *             one number into another.
- * @n: The first number.
- * @m: The second number.
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
  *
- * Return: The number of bits needed to flip.
+ * Return: number of bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned int bit_count = 0; /* Initialize the count of differing bits */
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-    /* Iterate through each bit of the numbers */
-    while (n || m)
-    {
-        /* Check if the least significant bits of both numbers differ */
-        if ((n & 1) != (m & 1))
-            bit_count++; /* Increment the count if the bits differ */
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
 
-        /* Right-shift both numbers to compare the next bit */
-        n >>= 1;
-        m >>= 1;
-    }
-
-    return bit_count; /* Return the count of differing bits */
+	return (count);
 }
+
 
