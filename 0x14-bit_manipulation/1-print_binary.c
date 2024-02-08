427beb2055f9e28063a0820c79b758d30de8eba4
mennassaber <mennasaber202620@gmail.com>
2024-02-08 14:49:20 -0800

    git

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,40 +1,27 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to print in binary.
- *
- * Return: No return value.
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-    /* Create a mask to isolate the rightmost bit */
-    unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
-    int flag = 0; /* Flag to track leading zeros */
+	int i, count = 0;
+	unsigned long int current;
 
-    /* Iterate through each bit of the number */
-    while (mask > 0)
-    {
-        /* Check if the current bit is set */
-        if (n & mask)
-        {
-            /* Set flag to indicate the first non-zero bit */
-            flag = 1;
-            /* Print '1' if the bit is set */
-            _putchar('1');
-        }
-        else if (flag) /* If leading zeros are already printed */
-        {
-            /* Print '0' if the bit is not set */
-            _putchar('0');
-        }
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
 
-        /* Shift the mask to the right by one bit */
-        mask >>= 1;
-    }
-
-    /* If the input number was zero, print '0' */
-    if (!flag)
-        _putchar('0');
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
 }
 
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,28 +1,21 @@
 #include "main.h"
 
 /**
- * get_bit - Get the value of a bit at a given index
- * @n: The number to extract the bit from
- * @index: The index of the bit to retrieve
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
  *
- * Return: The value of the bit at index, or -1 if an error occurred
+ * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    /*
-     * Check if index is within the bounds of the data type.
-     * If index is greater than or equal to the number of bits in an
-     * unsigned long int, return -1 indicating an error.
-     */
-    if (index >= (sizeof(unsigned long int) * 8))
-        return (-1);
+	int bit_val;
 
-    /*
-     * Right shift the number n by index bits, effectively bringing
-     * the bit at the specified index to the least significant position,
-     * and then perform a bitwise AND operation with 1 to retrieve
-     * the value of the bit.
-     */
-    return ((n >> index) & 1);
+	if (index > 63)
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
 
